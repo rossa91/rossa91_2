b23c4f087e63b95676ca6d8d3b8408f6e67bd460
김나성 <noela@Noelas-MacBook-Pro.local>
2020-02-29 17:24:55 +0900

    add QMobilenet

--- models/mobilenet.py
+++ models/mobilenet.py
@@ -6,7 +6,7 @@ for more details.
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from .module.quantize import *
 
 class Block(nn.Module):
     '''Depthwise conv + Pointwise conv'''
@@ -52,10 +52,57 @@ class MobileNet(nn.Module):
         return out
 
 
+class QBlock(nn.Module):
+    '''Depthwise conv + Pointwise conv'''
+    def __init__(self, in_planes, out_planes, stride=1, num_bits=4):
+        super(QBlock, self).__init__()
+        self.conv1 = QConv2d(in_planes, in_planes, kernel_size=3, stride=stride, padding=1, groups=in_planes, bias=False, num_bits = num_bits)
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv2 = QConv2d(in_planes, out_planes, kernel_size=1, stride=1, padding=0, bias=False, num_bits=num_bits)
+        self.bn2 = nn.BatchNorm2d(out_planes)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        return out
+
+class QMobileNet(nn.Module):
+    # (128,2) means conv planes=128, conv stride=2, by default conv stride=1
+    cfg = [64, (128,2), 128, (256,2), 256, (512,2), 512, 512, 512, 512, 512, (1024,2), 1024]
+
+    def __init__(self, num_classes=10, num_bits=8):
+        super(QMobileNet, self).__init__()
+        self.num_bits = num_bits
+        self.conv1 = QConv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False, num_bits=self.num_bits, num_bits_weight=self.num_bits)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.layers = self._make_layers(in_planes=32, num_bits=self.num_bits)
+        self.linear = QLinear(1024, num_classes, num_bits=self.num_bits)
+
+    def _make_layers(self, in_planes, num_bits):
+        layers = []
+        for x in self.cfg:
+            out_planes = x if isinstance(x, int) else x[0]
+            stride = 1 if isinstance(x, int) else x[1]
+            layers.append(QBlock(in_planes, out_planes, stride, num_bits))
+            in_planes = out_planes
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layers(out)
+        out = F.avg_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
 def test():
-    net = MobileNet()
+    net = QMobileNet(4)
     x = torch.randn(1,3,32,32)
     y = net(x)
     print(y.size())
 
-# test()
+#test()
+
+
+
