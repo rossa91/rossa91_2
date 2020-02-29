82e696d7e408f9466f8f64c8499e497d6ca8ad77
rossa91 <61299138+rossa91@users.noreply.github.com>
2020-03-01 00:55:39 +0900

    Update quantize.py

--- models/module/quantize.py
+++ models/module/quantize.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd.function import InplaceFunction, Function
 
-QParams = namedtuple('QParams', ['scale', 'zero_point', 'min_values', 'range'])
+QParams = namedtuple('QParams', ['range', 'zero_point', 'num_bits'])
 
 _DEFAULT_FLATTEN = (1, -1)
 _DEFAULT_FLATTEN_GRAD = (0, -1)
@@ -31,22 +31,9 @@ def calculate_qparams(x, num_bits, flatten_dims=_DEFAULT_FLATTEN, reduce_dim=0, 
                 min_values = min_values.min(reduce_dim, keepdim=keepdim)[0]
                 max_values = max_values.max(reduce_dim, keepdim=keepdim)[0]
         # TODO: re-add true zero computation
-        qmin = 0.
-        qmax = 2**num_bits -1
-        
-        range = (max_values - min_values) 
-        scale = (max_values - min_values) / (qmax - qmin)
-        
-        initial_zero_point = qmin - min_values / scale
-        if initial_zero_point < qmin :
-          nudged_zero_point = qmin
-        elif initial_zero_point > qmax : 
-          nudged_zero_point = qmax
-        else:
-          nudged_zero_point = initial_zero_point.round()
-          
-        return QParams(scale=scale, zero_point=nudged_zero_point, min_values=min_values, range=range)
-
+        range_values = max_values - min_values
+        return QParams(range=range_values, zero_point=min_values,
+                       num_bits=num_bits)
 
 class UniformQuantize(InplaceFunction):
 
@@ -67,14 +54,24 @@ class UniformQuantize(InplaceFunction):
             qparams = calculate_qparams(
                 input, num_bits=num_bits, flatten_dims=flatten_dims, reduce_dim=reduce_dim)
 
-        zero_point = qparams.zero_point
-        scale = qparams.scale
-        qmin = 0.
-        qmax = 2**num_bits -1 
-        
-        with torch.no_grad():
-            output.div_(qparams.scale).add_(qparams.zero_point)
+        qmin = -(2.**(num_bits - 1)) if signed else 0.
+        qmax = qmin + 2.**num_bits - 1.
+        scale = qparams.range / (qmax - qmin)
 
+
+        initial_zero_point = qmin - qparams.zero_point / scale
+        zero_point1 = torch.where(initial_zero_point < 0., qmin* torch.ones_like(initial_zero_point),  torch.zeros_like(initial_zero_point))
+        zero_point2 = torch.where(initial_zero_point > qmax, qmax*torch.ones_like(initial_zero_point), torch.zeros_like(initial_zero_point))
+        cond1 = initial_zero_point >= 0 
+        cond2 = initial_zero_point <= qmax
+        cond = cond1 & cond2
+        zero_point3 = torch.where(cond, initial_zero_point.round(), torch.zeros_like(initial_zero_point))
+        
+        zero_point = zero_point1 + zero_point2 + zero_point3
+        num_bits = qparams.num_bits
+
+        with torch.no_grad():
+            output.div_(scale).add_(qparams.zero_point)
             if stochastic:
                 noise = output.new(output.shape).uniform_(-0.5, 0.5)
                 output.add_(noise)
@@ -82,7 +79,7 @@ class UniformQuantize(InplaceFunction):
             output.clamp_(qmin, qmax).round_()
 
             if dequantize:
-              output.add_(-qparams.zero_point).mul_(qparams.scale)
+              output.add_(zero_point).mul_(scale)
         return output
 
     @staticmethod
@@ -91,64 +88,9 @@ class UniformQuantize(InplaceFunction):
         grad_input = grad_output
         return grad_input, None, None, None, None, None, None, None, None
 
-def quantize(x, num_bits=None, qparams=None, flatten_dims=_DEFAULT_FLATTEN, reduce_dim=0, dequantize=True, signed=False, stochastic=False, inplace=False):
+
+def quantize(x, num_bits=4, qparams=None, flatten_dims=_DEFAULT_FLATTEN, reduce_dim=0, dequantize=True, signed=False, stochastic=False, inplace=False):
     return UniformQuantize().apply(x, num_bits, qparams, flatten_dims, reduce_dim, dequantize, signed, stochastic, inplace)
-
-class MixedQuantize(InplaceFunction):
-
-    @staticmethod
-    def forward(ctx, input, mask, num_bits=None, qparams=None, flatten_dims=_DEFAULT_FLATTEN,
-                reduce_dim=0, dequantize=True, signed=False, stochastic=False, inplace=False):
-
-        ctx.inplace = inplace
-
-        if ctx.inplace:
-            ctx.mark_dirty(input)
-            output1 = input
-            output2 = input
-        else:
-            output1 = input.clone()
-            output2 = input.clone()
-
-        if qparams is None:
-            assert num_bits is not None, "either provide qparams of num_bits to quantize"
-            qparams = calculate_qparams(
-                input, num_bits=num_bits, flatten_dims=flatten_dims, reduce_dim=reduce_dim)
-
-        mask1 = mask
-        mask2 = 1 - mask
-
-        zero_point = qparams.zero_point
-        scale = qparams.scale
-        qmin = 0.
-        qmax = 2**num_bits -1 
-        
-        with torch.no_grad():
-            output1.div_(qparams.scale).add_(qparams.zero_point)
-            output2.div_(qparams.scale).add_(qparams.zero_point)
-
-            if stochastic:
-                noise = output1.new(output1.shape).uniform_(-0.5, 0.5)
-                output1.add_(noise)
-                output2.add_(noise)
-            # quantize
-            output1.clamp_(qmin, qmax).round_()
-            output2.clamp_(qmin, qmax).mul_(2).round_().div_(2)
-            output = mask1 * output1 + mask2 * output2
-
-            if dequantize:
-              output.add_(-qparams.zero_point).mul_(qparams.scale)
-
-        return output
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        # straight-through estimator
-        grad_input = grad_output
-        return grad_input, None, None, None, None, None, None, None, None
-
-def mixed_quantize(x, mask, num_bits=None, qparams=None, flatten_dims=_DEFAULT_FLATTEN, reduce_dim=0, dequantize=True, signed=False, stochastic=False, inplace=False):
-    return MixedQuantize().apply(x, mask, num_bits, qparams, flatten_dims, reduce_dim, dequantize, signed, stochastic, inplace)
 
 class QuantMeasure(nn.Module):
     """docstring for QuantMeasure."""
@@ -181,7 +123,7 @@ class QuantMeasure(nn.Module):
                 else:
                     momentum = self.momentum
                 self.running_zero_point.mul_(momentum).add_(
-                    qparams.min_values * (1 - momentum))
+                    qparams.zero_point * (1 - momentum))
                 self.running_range.mul_(momentum).add_(
                     qparams.range * (1 - momentum))
         else:
@@ -190,7 +132,7 @@ class QuantMeasure(nn.Module):
         if self.measure:
             return input
         else:
-            q_input = quantize(input, num_bits=self.num_bits, qparams=qparams, dequantize=self.dequantize,
+            q_input = quantize(input, qparams=qparams, dequantize=self.dequantize,
                                stochastic=self.stochastic, inplace=self.inplace)
             return q_input
 
@@ -246,6 +188,5 @@ class QLinear(nn.Linear):
         else:
             qbias = None
         return F.linear(qinput, qweight, qbias)
-
 
 
