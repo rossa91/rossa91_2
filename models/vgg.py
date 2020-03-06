9b3cfe80fa8007c3e9e424155092f852c2863f6e
rossa91 <61299138+rossa91@users.noreply.github.com>
2020-03-06 21:23:47 +0900

    Update vgg.py

--- models/vgg.py
+++ models/vgg.py
@@ -42,46 +42,55 @@ class VGG(nn.Module):
 
 
 class QVGG(nn.Module):
-    def __init__(self, vgg_name, num_bits, mixed=True):
+    def __init__(self, vgg_name, num_bits, mixed=True, mask=None):
         super(QVGG, self).__init__()
         self.num_bits = num_bits
         self.mixed = mixed
+        self.mask = mask 
         self.features = self._make_layers(cfg[vgg_name])
-        self.classifier = QLinear(512, 10, num_bits=self.num_bits, num_bits_weight=self.num_bits, mixed=self.mixed)
-
+        if self.mixed is True :
+          self.classifier = QLinear(512, 10, num_bits=self.num_bits, num_bits_weight=self.num_bits, mixed=self.mixed, mask=mask[len(mask)-1])
+        else :
+          self.classifier = QLinear(512, 10, num_bits=self.num_bits, num_bits_weight=self.num_bits, mixed=self.mixed, mask=None)
     def forward(self, x):
+        
         out = self.features(x)
         out = out.view(out.size(0), -1)
         out = self.classifier(out)
         return out
 
     def _make_layers(self, cfg):
+       
+        if self.mask is None and self.mixed is True:
+          print("Mask Error... insert the mask")
+       
         layers = []
         in_channels = 3
+        i = 0
         for x in cfg:
             if x == 'M':
                 layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
             else:
-                layers += [QConv2d(in_channels, x, kernel_size=3, padding=1, num_bits=self.num_bits, num_bits_weight=self.num_bits, mixed=self.mixed),
+                if self.mixed is False :
+                  mask=None
+                else:
+                  mask=self.mask[i]
+
+                layers += [QConv2d(in_channels, x, kernel_size=3, padding=1, num_bits=self.num_bits, num_bits_weight=self.num_bits, mixed=self.mixed, mask=mask),
                            nn.BatchNorm2d(x),
                            nn.ReLU(inplace=True)]
                 in_channels = x
+                i += 1
         layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
         return nn.Sequential(*layers)
 
 
 
-
-
-
-
-
-
-
 def test():
-    net = VGG('VGG11')
+    net = QVGG('VGG9', 4, mixed=False, mask=None)
     x = torch.randn(2,3,32,32)
     y = net(x)
     print(y.size())
+
 
 # test()
